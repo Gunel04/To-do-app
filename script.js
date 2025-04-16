3bf36a63d718ced8f9a88206280fa1bd9e47d96b
Gunel04 <alkhasovagunel@gmail.com>
2025-04-16 16:11:06 +0400

    to-do-app

---
+++ script.js
@@ -0,0 +1,42 @@
+const addTodo = document.querySelector('.add-todo');
+const todoList = document.querySelector('.todo-list');
+const todoInput = document.querySelector('.todo-input');
+const clearAll = document.querySelector('.clear-all');
+
+let count = 0;
+addTodo.onclick = (e) => {
+    e.preventDefault();
+    const todoListItem = document.createElement('li');
+    const taskInfo = document.querySelector('.task-info');
+    if (todoInput.value !== "") {
+        todoListItem.innerHTML = `${todoInput.value} <button class="del-btn"><i class="fa-solid fa-trash"></i></button>`;
+        todoList.appendChild(todoListItem);
+        todoInput.value = "";
+        count++;
+        if(count === 1) {
+            taskInfo.innerHTML = `You have ${count} pending task`;
+        }
+        else {taskInfo.innerHTML = `You have ${count} pending tasks`;}
+        
+    }
+
+    const delBtn = document.querySelector('.del-btn');
+    todoListItem.onclick = () => {
+        todoListItem.remove();
+        count--;
+        if(count === 1) {
+            taskInfo.innerHTML = `You have ${count} pending task`;
+        } else if(count === 0) {
+            taskInfo.innerHTML = `You have no any task`;
+        } else
+        taskInfo.innerHTML = `You have ${count} pending tasks`;
+
+    }
+
+    clearAll.onclick = () => {
+        todoList.innerHTML = "";
+        count = 0;
+        taskInfo.innerHTML = `You have no any task`;
+    }
+}
+
